d576e6a9a1dc6abb86c081b7696891d7b84e5673
Verse <verseow@gmail.com>
2022-06-30 22:30:02 -0700

    created structs

--- api/routes/shortener.go
+++ api/routes/shortener.go
@@ -1,2 +1,17 @@
 package routes
 
+import "time"
+
+type res struct { 
+	URL string `json:"url"`
+	Shortened string `json:"shortened"`
+	Expiration time.Duration `json:"expiration"`
+	RateLimit int `json:"rate_limit"`
+	RateReset time.Duration  `json:"rate_reset"`
+}
+
+type req struct {
+	URL string `json:"url"`
+	Shortened string `json:"shortened"`
+	Expiration time.Duration `json:"expiration"`
+}
