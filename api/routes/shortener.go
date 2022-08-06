e0416ed4002c42ce3448ab5b3c5342b99b22904d
Verse <verseow@gmail.com>
2022-08-05 20:28:01 -0400

    created http enforce

--- api/routes/shortener.go
+++ api/routes/shortener.go
@@ -1,6 +1,9 @@
 package routes
 
-import "time"
+import (
+	"time"
+	"strings"
+)
 
 type res struct { 
 	URL string `json:"url"`
@@ -14,4 +17,33 @@ type req struct {
 	URL string `json:"url"`
 	Shortened string `json:"shortened"`
 	Expiration time.Duration `json:"expiration"`
+}
+
+func HTTP(url string) string {
+	if !strings.HasPrefix(url, "http://") {
+		url = "http://" + url
+	}
+	return url
+}
+
+
+func shorten(c *fiber.Ctx) error {
+
+	body := new(req)
+	if err := c.BodyParser(&body); err != nil {
+		return err
+	}
+
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(400).JSON(res{
+			URL: body.URL,
+			Shortened: "",
+			Expiration: 0,
+			RateLimit: 0,
+			RateReset: 0,
+		})
+	}
+
+	body.URL = HTTPS(body.URL)
+
 }
